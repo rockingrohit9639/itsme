18dac40c09f3ba951f1541fe3e07da9820f3cbc4
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-12 21:10:20 +0530

    implemented Context for sharing user info

--- src/App.js
+++ src/App.js
@@ -2,36 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
-import { auth } from "./firebase";
+import { AuthProvider } from "./Contexts/AuthContext";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
-
-  auth.onAuthStateChanged((user) => {
-    if (user) {
-      return setIsAuthenticated(true);
-    } else {
-      return setIsAuthenticated(false);
-    }
-  });
-
-  if (isAuthenticated) {
-    return (
+  return (
+    <AuthProvider>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
-    );
-  } else {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-        </Routes>
-      </Router>
-    );
-  }
+    </AuthProvider>
+  );
 }
 
 export default App;
