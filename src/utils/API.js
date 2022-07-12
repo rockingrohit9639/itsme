128c2664515eb9f72718ddbaabaae69215aa9094
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-12 22:12:59 +0530

    Created function for getting links for a user

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,15 @@
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc } from "firebase/firestore";
 import { db } from "../firebase";
 
 export const addNewLink = (username, title, url) => {
   const docRef = doc(db, "users", username);
 
   return setDoc(docRef, { [title]: url });
+};
+
+export const getLinks = async (username) => {
+  const docRef = doc(db, "users", username);
+  const userDoc = await getDoc(docRef);
+
+  return userDoc.data();
 };
