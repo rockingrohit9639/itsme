13ed5a962a0dcbabe04a8ae5ef68962c7291e0a4
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-12 21:18:05 +0530

    removed context

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
-import { AuthProvider } from "./Contexts/AuthContext";
+import { auth } from "./firebase";
 
 function App() {
-  return (
-    <AuthProvider>
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (isAuthenticated) {
+    return (
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
-    </AuthProvider>
-  );
+    );
+  } else {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
