d53b660f48666b6d672cc7b53520ad802e414faf
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-15 21:31:20 +0530

    added feature to add a new link

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,27 +1,52 @@
-import React from "react";
-import LinkCard from "../LinkCard/LinkCard";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { addNewLink } from "../../utils/API";
 import "./Home.css";
 
 function Home() {
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const username = useSelector((state) => state.user?.user?.username);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!title || !url) {
+      return;
+    }
+
+    try {
+      await addNewLink(username, title.toLowerCase(), url);
+      setTitle("");
+      setUrl("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="home container">
-      <form className="inputForm">
-        <input type="text" placeholder="Enter title" className="input" />
-        <input type="text" placeholder="Enter URL" className="input" />
+      <form className="inputForm" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter title"
+          className="input"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Enter URL"
+          className="input"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
 
         <button type="submit" className="btn">
           Add Link
         </button>
       </form>
 
-      <div className="home__content">
-        <LinkCard />
-        <LinkCard />
-        <LinkCard />
-        <LinkCard />
-        <LinkCard />
-        <LinkCard />
-      </div>
+      <div className="home__content"></div>
     </div>
   );
 }
