c5ad4578bea42b41172f5c15f28ffe1e88c27496
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-15 20:09:24 +0530

    Updated Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,21 +5,21 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="container d-flex align-items-center">
-        <div className="navbar__left">
-          <Link to={"/"} className="nav-link">
-            <h1 className="logo">itsme</h1>
-          </Link>
-        </div>
-        <div className="navbar__right d-flex">
-          <img
-            src="https://variety.com/wp-content/uploads/2022/02/Screen-Shot-2022-05-09-at-10.04.13-AM.png"
-            alt="avatar"
-            className="navbar__userImg img-fluid img-thumbnail"
-          />
+      <div className="navbar__left">
+        <Link to={"/"} className="nav-link">
+          <h1 className="logo">itsme</h1>
+        </Link>
+      </div>
+      <div className="navbar__right d-flex">
+        <img
+          src="https://variety.com/wp-content/uploads/2022/02/Screen-Shot-2022-05-09-at-10.04.13-AM.png"
+          alt="avatar"
+          className="navbar__userImg img-fluid img-thumbnail"
+        />
 
-          <Link to={"/logout"} className="nav-link">Logout</Link>
-        </div>
+        <Link to={"/logout"} className="nav-link">
+          Logout
+        </Link>
       </div>
     </div>
   );
