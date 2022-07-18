d3cb1f117d4053169d1c9ea798a6d9be03901fc5
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-18 11:11:29 +0530

    showing link and added button to copy link

--- src/Components/LinkCard/LinkCard.js
+++ src/Components/LinkCard/LinkCard.js
@@ -3,6 +3,8 @@ import "./LinkCard.css";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { getLinkIcons } from "../../utils/utils";
 import InsertLinkIcon from "@mui/icons-material/InsertLink";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { Tooltip } from "@mui/material";
 import { deleteLink } from "../../utils/API";
 import { useSelector, useDispatch } from "react-redux";
 import { deleteUserLink } from "../../redux/userRedux";
@@ -30,19 +32,37 @@ function LinkCard({ title, url, username }) {
     }
   };
 
+  const handleCopyToClipboard = () => {
+    window.navigator.clipboard.writeText(
+      `${window.location.origin}/${username}/${title}`
+    );
+    alert("Copied to clipboard!");
+  };
+
   return (
     <div className="linkCard">
       {getLinkIcons(title)}
 
       <div className="linkCard__title">{title}</div>
 
+      <p className="linkCard__link">{`/${username}/${title}`}</p>
+
       <div className="linkCard__actionButtons">
+        <div className="icon" onClick={handleCopyToClipboard}>
+          <Tooltip title="Copy Link">
+            <ContentCopyIcon />
+          </Tooltip>
+        </div>
         <div className="icon" onClick={() => redirectURL(url)}>
-          <InsertLinkIcon />
+          <Tooltip title="Visit Link">
+            <InsertLinkIcon />
+          </Tooltip>
         </div>
         {isAuthenticated && username === userFromStore ? (
           <div className="icon" onClick={handleDelete}>
-            <DeleteIcon sx={{ color: "red" }} />
+            <Tooltip title="Delete Link">
+              <DeleteIcon sx={{ color: "red" }} />
+            </Tooltip>
           </div>
         ) : null}
       </div>
